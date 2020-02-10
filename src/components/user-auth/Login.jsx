86245731c14c2cf3cef49b2c9bb5fbf8225274ea
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-02-10 00:12:19 -0500

    completed login page

--- src/components/user-auth/Login.jsx
+++ src/components/user-auth/Login.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import UserActions from "../../actions/loginReducer";
 
 import "./UserAuth.scss";
 
-export const Login = () => {
+export const Login = props => {
+	const dispatch = useDispatch();
+	const [loginInput, setLoginInput] = useState({
+		username: "",
+		password: ""
+	});
+	const handleChange = e => {
+		setLoginInput({
+			...loginInput,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		dispatch(UserActions.loginUserToDB(loginInput));
+		props.history.push("/");
+	};
 	return (
 		<div>
-			<form onSubmit={null}>
+			<form onSubmit={handleSubmit}>
 				<input
 					type="text"
 					placeholder="Username"
-					value={null}
-					handleChange={null}
+					name="username"
+					value={loginInput.username}
+					handleChange={handleChange}
 				/>
 				<input
 					type="text"
 					placeholder="Password"
-					value={null}
-					handleChange={null}
+					name="password"
+					value={loginInput.password}
+					handleChange={handleChange}
 				/>
 				<Button variant="outlined" type="submit">
 					Login
